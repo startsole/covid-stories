2b68ec92b099654cf3f911b44b59c3e12deadc45
Justin Duffy <jsduffy@gmail.com>
2021-01-06 14:21:09 -0500

    keep on party rocking

--- src/views/Stories/MsCarlene.js
+++ src/views/Stories/MsCarlene.js
@@ -2,11 +2,9 @@ import React from 'react';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
 import {colors, Grid, GridList, GridListTile, IconButton, Typography, useMediaQuery} from '@material-ui/core';
 import Section from "../../components/organisms/Section";
-import Content from "../BlogArticle/components/Content";
 import SidebarArticles from "../BlogArticle/components/SidebarArticles";
 import SidebarNewsletter from "../BlogArticle/components/SidebarNewsletter/SidebarNewsletter";
 import Hero from "../BlogArticle/components/Hero";
-import clsx from "clsx";
 import {Image} from "../../components/atoms";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
@@ -64,24 +62,7 @@ const content = {
   cover: {
     src: '/covid-stories/images/photos/blog/ms-carlene/holding-hands.jpg',
     srcSet: '/covid-stories/images/photos/blog/ms-carlene/holding-hands.jpg 2x',
-  },
-  images: [
-    {
-      src: '/covid-stories/images/photos/blog/cover4.jpg',
-      srcSet: '/covid-stories/images/photos/blog/cover4.jpg 2x',
-      cols: 2,
-    },
-    {
-      src: '/covid-stories/images/photos/blog/cover3.jpg',
-      srcSet: '/covid-stories/images/photos/blog/cover3.jpg 2x',
-      cols: 1,
-    },
-    {
-      src: '/covid-stories/images/photos/blog/cover1.jpg',
-      srcSet: '/covid-stories/images/photos/blog/cover1.jpg 2x',
-      cols: 1,
-    },
-  ],
+  }
 };
 
 const sidebarArticles = [
@@ -173,40 +154,57 @@ const StoryMsCarlene = () => {
             {/*<Content data={content} />*/}
 
             {/*<div className={clsx(classes.root, className)} {...rest}>*/}
-              <div className={classes.section}>
-                <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                  There are many people who are scared of Covid-19. Millions around the world have it, are recovering from it, or are dying from it. Today, we are focusing on one person that lived to tell the tale of how she survived the pandemic of this generation. Her name is simply, Ms. Carlene.
-                </Typography>
-              </div>
+            <div className={classes.section}>
+              <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
+                There are many people who are scared of Covid-19. Millions around the world have it, are recovering from
+                it, or are dying from it. Today, we are focusing on one person that lived to tell the tale of how she
+                survived the pandemic of this generation. Her name is simply, Ms. Carlene.
+              </Typography>
+            </div>
             <div className={classes.section}>
               <Image
                 src={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs.jpg'}
                 srcSet={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs.jpg 2x'}
                 className={classes.image}
-                lazyProps={{ width: '100%', height: '100%' }}
+                lazyProps={{width: '100%', height: '100%'}}
               />
             </div>
             <div className={classes.section}>
               <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                The first thing I noticed as our video call with Ms. Carlene was beginning is how the staff at Sycamore Springs Assisted Living Center, the place where Ms. Carlene now calls home, handled things. I could see Jordan Bishop, the resident life coordinator, walking to Ms. Carlene's room, and an older man was in his doorway asking for lunch. Mr. Bishop jumped straight on it; making sure he got everything and relaying straight to someone over the radio to get it to him.
+                The first thing I noticed as our video call with Ms. Carlene was beginning is how the staff at Sycamore
+                Springs Assisted Living Center, the place where Ms. Carlene now calls home, handled things. I could see
+                Jordan Bishop, the resident life coordinator, walking to Ms. Carlene's room, and an older man was in his
+                doorway asking for lunch. Mr. Bishop jumped straight on it; making sure he got everything and relaying
+                straight to someone over the radio to get it to him.
               </Typography>
             </div>
             {/*quote*/}
             <Box mt={6} mb={6}>
               <div className={classes.section}>
                 <Typography component="p" variant="h4" color="primary" align="center" gutterBottom={true}>
-                  "I can't run on the treadmill now because of this pandemic. I also can't see my daughter, she can only come up to my window and talk."
+                  "I can't run on the treadmill now because of this pandemic. I also can't see my daughter, she can only
+                  come up to my window and talk."
                 </Typography>
               </div>
             </Box>
             <div className={classes.section}>
               <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                At the beginning of the pandemic, there were often problems with people not being able to keep in touch with their family members in these homes. Many extended care facilities across America have had issues with communication between their residents and their families. Some families never even knew their family members had Covid until they were already dead. Ms. Carlene says that Sycamore Springs did not have those issues. They battled the red tape to take care of their residents and make sure that they got the help they needed.
+                At the beginning of the pandemic, there were often problems with people not being able to keep in touch
+                with their family members in these homes. Many extended care facilities across America have had issues
+                with communication between their residents and their families. Some families never even knew their
+                family members had Covid until they were already dead. Ms. Carlene says that Sycamore Springs did not
+                have those issues. They battled the red tape to take care of their residents and make sure that they got
+                the help they needed.
               </Typography>
             </div>
             <div className={classes.section}>
               <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                I learned that Ms. Carlene used to be a teacher at Hunter Elementary School in Elizabethton, TN. She even taught my current high school principal, Jon Minton. “He was a great student,” she recalls. Ms. Carlene lives in the assisted living facility just 2 blocks from my school, Elizabethton High. Despite being over 90 years old, Ms. Carlene used to work out every day until the Covid pandemic swept over the assisted living center. “I can't run on the treadmill now because of this pandemic. I also can't see my daughter, she can only come up to my window and talk.”
+                I learned that Ms. Carlene used to be a teacher at Hunter Elementary School in Elizabethton, TN. She
+                even taught my current high school principal, Jon Minton. “He was a great student,” she recalls. Ms.
+                Carlene lives in the assisted living facility just 2 blocks from my school, Elizabethton High. Despite
+                being over 90 years old, Ms. Carlene used to work out every day until the Covid pandemic swept over the
+                assisted living center. “I can't run on the treadmill now because of this pandemic. I also can't see my
+                daughter, she can only come up to my window and talk.”
               </Typography>
             </div>
             <div className={classes.section}>
@@ -215,66 +213,79 @@ const StoryMsCarlene = () => {
                 cols={2}
                 spacing={isMd ? 24 : 8}
               >
-                {/*{content.images.map((item, index) => (*/}
-                  <GridListTile key={1} cols={isMd ? 1 : 2}>
-                    <Image
-                      src={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-assisted-living.jpeg'}
-                      srcSet={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-assisted-living.jpeg 2x'}
-                      className={classes.image}
-                      lazyProps={{ width: '100%', height: '100%' }}
-                    />
-                  </GridListTile>
-                  <GridListTile key={2} cols={isMd ? 1 : 2}>
-                    <Image
-                      src={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-bedroom.jpg'}
-                      srcSet={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-bedroom.jpg 2x'}
-                      className={classes.image}
-                      lazyProps={{ width: '100%', height: '100%' }}
-                    />
-                  </GridListTile>
-                {/*))}*/}
+                <GridListTile key={1} cols={isMd ? 1 : 2}>
+                  <Image
+                    src={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-assisted-living.jpeg'}
+                    srcSet={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-assisted-living.jpeg 2x'}
+                    className={classes.image}
+                    lazyProps={{width: '100%', height: '100%'}}
+                  />
+                </GridListTile>
+                <GridListTile key={2} cols={isMd ? 1 : 2}>
+                  <Image
+                    src={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-bedroom.jpg'}
+                    srcSet={'/covid-stories/images/photos/blog/ms-carlene/sycamore-springs-bedroom.jpg 2x'}
+                    className={classes.image}
+                    lazyProps={{width: '100%', height: '100%'}}
+                  />
+                </GridListTile>
               </GridList>
             </div>
             <div className={classes.section}>
               <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                When Ms. Carlene contracted Covid and went to the hospital, she was in the Covid Unit at Johnson City Medical Center in Johnson City, TN for two weeks. She believes she got Covid because of her breathing problems. They hooked her up to many machines at the hospital to keep her healthy. The hospital was so full that they had to move people down into the basement, which is where she was. She says that despite being tucked away below ground, the staff was on top of everything. When she asked for anything, they got it for her.
+                When Ms. Carlene contracted Covid and went to the hospital, she was in the Covid Unit at Johnson City
+                Medical Center in Johnson City, TN for two weeks. She believes she got Covid because of her breathing
+                problems. They hooked her up to many machines at the hospital to keep her healthy. The hospital was so
+                full that they had to move people down into the basement, which is where she was. She says that despite
+                being tucked away below ground, the staff was on top of everything. When she asked for anything, they
+                got it for her.
               </Typography>
             </div>
             <div className={classes.section}>
               <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                When she got back to Sycamore Springs, she felt just about back to normal, but she said her lungs are worse now because of Covid-19. They continue to keep the residents of Sycamore Springs safe when they talk to each other, by having them wear masks. She said that when it got bad the first time, they wore full protective suits. She said if it gets bad again, they would be ready to take full precautions, unlike the first wave.
+                When she got back to Sycamore Springs, she felt just about back to normal, but she said her lungs are
+                worse now because of Covid-19. They continue to keep the residents of Sycamore Springs safe when they
+                talk to each other, by having them wear masks. She said that when it got bad the first time, they wore
+                full protective suits. She said if it gets bad again, they would be ready to take full precautions,
+                unlike the first wave.
               </Typography>
             </div>
             <div className={classes.section}>
               <Typography component="p" variant="h6" color="textPrimary" gutterBottom={true}>
-                Ms. Carlene survived the deadliest pandemic in the last 100 years. She already had lung problems, but she fought through those issues and lived to tell me her story. Without these stories people would not know what many of the residents in extended care facilities, like Ms. Carlene, went through and the struggles they endured. She said that during the early days of the pandemic the residents were scared for their lives and never knew if they were going to see their family again. Happily, she did survive, and does get to see her daughter again...even if it has been almost a year, and she only gets to whisper at her through the window. She may have endured Covid-19, but the disease continues to plague her, and millions more, by separating her from the ones she loves. Despite her age, she shook off the disease in only a couple of weeks, but no one yet knows how long she will continue to be a victim of the pandemic.
+                Ms. Carlene survived the deadliest pandemic in the last 100 years. She already had lung problems, but
+                she fought through those issues and lived to tell me her story. Without these stories people would not
+                know what many of the residents in extended care facilities, like Ms. Carlene, went through and the
+                struggles they endured. She said that during the early days of the pandemic the residents were scared
+                for their lives and never knew if they were going to see their family again. Happily, she did survive,
+                and does get to see her daughter again...even if it has been almost a year, and she only gets to whisper
+                at her through the window. She may have endured Covid-19, but the disease continues to plague her, and
+                millions more, by separating her from the ones she loves. Despite her age, she shook off the disease in
+                only a couple of weeks, but no one yet knows how long she will continue to be a victim of the pandemic.
               </Typography>
             </div>
 
 
-              <div>
-                <IconButton className={classes.socialIcon}>
-                  <FacebookIcon />
-                </IconButton>
-                <IconButton className={classes.socialIcon}>
-                  <InstagramIcon />
-                </IconButton>
-                <IconButton className={classes.socialIcon}>
-                  <TwitterIcon />
-                </IconButton>
-                <IconButton className={classes.socialIcon}>
-                  <PinterestIcon />
-                </IconButton>
-              </div>
+            <div>
+              <IconButton className={classes.socialIcon}>
+                <FacebookIcon/>
+              </IconButton>
+              <IconButton className={classes.socialIcon}>
+                <InstagramIcon/>
+              </IconButton>
+              <IconButton className={classes.socialIcon}>
+                <TwitterIcon/>
+              </IconButton>
+              <IconButton className={classes.socialIcon}>
+                <PinterestIcon/>
+              </IconButton>
+            </div>
             {/*</div>*/}
-
-
 
 
           </Grid>
           <Grid item xs={12} md={4}>
-            <SidebarArticles data={sidebarArticles} />
-            <SidebarNewsletter className={classes.sidebarNewsletter} />
+            <SidebarArticles data={sidebarArticles}/>
+            <SidebarNewsletter className={classes.sidebarNewsletter}/>
           </Grid>
         </Grid>
       </Section>
