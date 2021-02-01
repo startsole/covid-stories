7e6b0128775edc954d8abca7bb3b226b029b77e4
Justin Duffy <jsduffy@gmail.com>
2021-02-01 13:14:36 -0500

    photo update

--- src/views/Stories/MsCarlene.js
+++ src/views/Stories/MsCarlene.js
@@ -202,10 +202,11 @@ const StoryMsCarlene = () => {
               >
                 <GridListTile key={1} cols={isMd ? 1 : 2}>
                   <Image
-                    src={'/covid-stories/images/photos/blog/ms-carlene/carlene-4.png'}
-                    srcSet={'/covid-stories/images/photos/blog/ms-carlene/carlene-4.png 2x'}
+                    src={'/covid-stories/images/photos/blog/ms-carlene/carlene-4.jpg'}
+                    srcSet={'/covid-stories/images/photos/blog/ms-carlene/carlene-4.jpg 2x'}
                     className={classes.image}
                     lazyProps={{width: '100%', height: '100%'}}
+                    alt={'Ms. Carlene with the Elizabethton High School principal Jon Minton, one of her former students'}
                   />
                 </GridListTile>
                 <GridListTile key={2} cols={isMd ? 1 : 2}>
@@ -214,7 +215,6 @@ const StoryMsCarlene = () => {
                     srcSet={'/covid-stories/images/photos/blog/ms-carlene/carlene-2.png 2x'}
                     className={classes.image}
                     lazyProps={{width: '100%', height: '100%'}}
-                    alt={'Ms. Carlene with the Elizabethton High School principal Jon Minton, one of her former students'}
                   />
                 </GridListTile>
               </GridList>
